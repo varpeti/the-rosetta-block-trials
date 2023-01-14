031f013c0c2d1b9e4a587fb2c4cfd6d50711fe48
varpeti <peti.var97@gmail.com>
2023-01-14 16:21:11 +0100

    level_id, panel_id; end screen

--- src/components/selector.rs
+++ src/components/selector.rs
@@ -2,10 +2,12 @@ use yew::prelude::*;
 
 use crate::components::level::*;
 
+const EXTRA_PANELS_BEFORE: usize = 2;
+
 #[function_component(Selector)]
 pub fn show_level_selector() -> Html {
-    let level_id = use_state(|| 2); // Stores the current level id
-    let unlocked_level = use_state(|| 2); // Stores the higest level unlocked
+    let panel_id = use_state(|| EXTRA_PANELS_BEFORE); // Stores the current level id
+    let unlocked_level = use_state(|| 0); // Stores the higest level unlocked
     let code = use_state(|| vec![]); // Stores the current code entered by the player
     let indicator = use_state(|| false); // Indicate the last answer correctness
 
@@ -38,17 +40,21 @@ pub fn show_level_selector() -> Html {
         // TODO add levels
     ];
 
+    let mut level_id: Option<usize> = None;
+    if *panel_id >= EXTRA_PANELS_BEFORE && *panel_id < EXTRA_PANELS_BEFORE + levels.len() {
+        level_id = Some(*panel_id - EXTRA_PANELS_BEFORE);
+    }
+
     // TODO keyboard support
 
     let forward = {
-        let level_id = level_id.clone();
+        let panel_id = panel_id.clone();
         let unlocked_level = unlocked_level.clone();
         let code = code.clone();
         let indicator = indicator.clone();
-        let levels_len = levels.len();
         Callback::from(move |_| {
-            if *level_id + 1 <= *unlocked_level && *level_id < levels_len - 1 {
-                level_id.set(*level_id + 1);
+            if level_id.is_none() || level_id.expect("level_id") < *unlocked_level {
+                panel_id.set(*panel_id + 1);
                 code.set(vec![]);
                 indicator.set(false);
             }
@@ -56,19 +62,19 @@ pub fn show_level_selector() -> Html {
     };
 
     let forward_button;
-    if *level_id + 1 <= *unlocked_level {
+    if level_id.is_none() || level_id.expect("level_id") < *unlocked_level {
         forward_button = html! { <td class={"nav"}> <span class={"nav_button"} onclick={forward}>{'>'}</span> </td> };
     } else {
         forward_button = html! { <td class={"nav"}></td> }
     }
 
     let backward = {
-        let level_id = level_id.clone();
+        let panel_id = panel_id.clone();
         let code = code.clone();
         let indicator = indicator.clone();
         Callback::from(move |_| {
-            if *level_id > 0 {
-                level_id.set(*level_id - 1);
+            if *panel_id > 0 {
+                panel_id.set(*panel_id - 1);
                 code.set(vec![]);
                 indicator.set(false);
             }
@@ -76,30 +82,30 @@ pub fn show_level_selector() -> Html {
     };
 
     let backward_button;
-    if *level_id > 0 {
+    if *panel_id > 0 {
         backward_button = html! { <td class={"nav"}> <span class={"nav_button"} onclick={backward}>{'<'}</span> </td> };
     } else {
         backward_button = html! { <td class={"nav"}></td> }
     }
 
     let unlock = {
-        let level_id = level_id.clone();
         let unlocked_level = unlocked_level.clone();
         Callback::from(move |_| {
-            if *level_id + 1 > *unlocked_level {
-                unlocked_level.set(*level_id + 1);
+            if let Some(level_id) = level_id {
+                if level_id >= *unlocked_level {
+                    unlocked_level.set(level_id + 1);
+                }
             }
         })
     };
 
-    if *level_id == 0 {
+    if *panel_id == 0 {
         return html! {
             <table>
                 <tr>
-                    {backward_button}
                     <table>
                         <tr> <th colspan="2"> {"Credit"} </th> </tr>
-                        <tr> 
+                        <tr>
                             <td class="credit">{"Puzzle creator:"}</td>
                             <td class="credit"><a href="https://mmcelebration.com/level/4/31/" target="_blank">{"rubenscube"}</a></td>
                         </tr>
@@ -122,22 +128,35 @@ pub fn show_level_selector() -> Html {
         };
     }
 
-    if *level_id == 1 {
+    if *panel_id == 1 {
         return html! {
             <table>
                 <tr>
                     {backward_button}
                         <p> {"Achievements"} </p> //TODO
-                        <p> {*unlocked_level-2}{"/"}{levels.len()} </p>
+                        <p> {*unlocked_level}{"/"}{levels.len()} </p>
                     {forward_button}
                 </tr>
             </table>
         };
     }
 
+    if level_id.is_none() { // All levels are done
+        return html! {
+            <table>
+                <tr>
+                    {backward_button}
+                        <p> {"Congratulation!"} </p>
+                        <p> {*unlocked_level}{"/"}{levels.len()} </p>
+                </tr>
+            </table>
+
+        } 
+    }
+
     let level = levels
-        .get(*level_id - 2)
-        .expect("The {level_id} level does not exists!")
+        .get(level_id.expect(format!("The level_id is None. The panel_id is {}.", *panel_id).as_str() ))
+        .expect(format!("The {} level does not exists!", level_id.expect("level_id")).as_str())
         .clone();
 
     html! {
